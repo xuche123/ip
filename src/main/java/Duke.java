dee4769c547032ee0b3a42c4974d322ad291d2e3
xuche123 <xuche1998@gmail.com>
2020-08-16 17:27:17 +0800

    add increment 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,5 +5,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
